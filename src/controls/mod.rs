4f0bf52c0b137e04295dc059b2940b2788ca55ad
gnik-snrub <60440393+gnik-snrub@users.noreply.github.com>
2020-04-26 21:41:42 +1000

    Adjusted controls to allow for diagonal movement, and to wait a turn

--- src/controls/mod.rs
+++ src/controls/mod.rs
@@ -22,20 +22,39 @@ pub fn handle_keys(tcod: &mut Tcod, game: &Game, objects: &mut [Object]) -> Play
     let player_alive = objects[PLAYER].alive;
     match (key, key.text(), player_alive) {
         // Movement keys
+        ( Key { code: NumPad7, .. }, _, true) => {
+            Object::player_move_or_attack(-1, -1, game, objects);
+            TookTurn
+        },
         ( Key { code: NumPad8, .. }, _, true) => {
             Object::player_move_or_attack(0, -1, game, objects);
             TookTurn
         },
-        ( Key { code: NumPad5, .. }, _, true) => {
-            Object::player_move_or_attack(0, 1, game, objects);
+        ( Key { code: NumPad9, .. }, _, true) => {
+            Object::player_move_or_attack(1, -1, game, objects);
             TookTurn
         },
         ( Key { code: NumPad4, .. }, _, true) => {
             Object::player_move_or_attack(-1, 0, game, objects);
             TookTurn
         },
+        ( Key { code: NumPad5, .. }, _, true) => {
+            TookTurn // Wait a turn
+        },
         ( Key { code: NumPad6, .. }, _, true) => {
             Object::player_move_or_attack(1, 0, game, objects);
+            TookTurn
+        },
+        ( Key { code: NumPad1, .. }, _, true) => {
+            Object::player_move_or_attack(-1, 1, game, objects);
+            TookTurn
+        },
+        ( Key { code: NumPad2, .. }, _, true) => {
+            Object::player_move_or_attack(0, 1, game, objects);
+            TookTurn
+        },
+        ( Key { code: NumPad3, .. }, _, true) => {
+            Object::player_move_or_attack(1, 1, game, objects);
             TookTurn
         },
 //      This code is temporarily removed, as it breaks the laptop on which it is being written.
